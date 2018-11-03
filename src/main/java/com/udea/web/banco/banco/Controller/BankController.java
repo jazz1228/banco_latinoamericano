b957c051456b3441eb84a9d66a0348b0dea68fdb
andres0510 <j.andres0510@hotmail.com>
2018-11-03 09:49:30 -0500

    correccion registro

--- src/main/java/com/udea/web/banco/banco/Controller/BankController.java
+++ src/main/java/com/udea/web/banco/banco/Controller/BankController.java
@@ -242,6 +242,8 @@ public class BankController {
 
         try {
             JSONObject obj=new JSONObject(registro);
+            User usr1;
+            User usr2;
 
             //Json a java
             id = obj.getString("id");
@@ -255,6 +257,12 @@ public class BankController {
             tipoCuenta = obj.getString("tipoCuenta");
             passAccount = obj.getString("passwordCuenta");
             Country pais = countryRepository.findByName(country);
+
+            usr1 = userRepository.findUserByEmail(email);
+            usr2 = userRepository.findByUid(id);
+            if(usr1 != null || usr2 != null){
+                return "Fallo";
+            }
 
             numberAccount = createNumberAccount();
 
