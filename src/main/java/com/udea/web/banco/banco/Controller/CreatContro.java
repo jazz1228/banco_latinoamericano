8348a1213c3e0431139efa89f165f861d538d92d
juan alberto zapata zapata <juan.zapata@sofka.com.co>
2019-02-18 15:04:28 -0500

    connect to mongoAtlas

--- src/main/java/com/udea/web/banco/banco/Controller/CreatContro.java
+++ src/main/java/com/udea/web/banco/banco/Controller/CreatContro.java
@@ -13,6 +13,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 
 @RestController
 @RequestMapping("/v1/prueba")
@@ -28,37 +30,7 @@ public class CreatContro {
     CountryRepository countryRepository;
 
     @GetMapping("/prueba1")
-    public String prueba1(){
-        User user=new User();
-        Country country=new Country();
-        Account account=new Account();
-
-        account.setNumber("1234341231");
-        account.setBalance(12312312.21);
-        account.setPass("1234");
-        account.setType("ahorro");
-        account.setId(ObjectId.get());
-        accountRepository.save(account);
-
-        country.setName("Colombia");
-        country.setCoin("COP");
-        country.setId(ObjectId.get());
-        countryRepository.save(country);
-
-        user.setAddress("xxx-xxx");
-        user.setBirthDay("12-28-1995");
-        user.setCountry(country);
-        user.setEmail("Juan@hotmail.com");
-        user.setId("123456789");
-        user.setName("Juan");
-        user.setPass("123456");
-        user.setPhone("320182321");
-        user.setRole("Contador");
-        user.setNumberAccount(account);
-        user.setIden(ObjectId.get());
-        userRepository.save(user);
-
-
-        return "Funciono";
+    public List<User> prueba1(){
+       return userRepository.findAll();
     }
 }
--- src/test/java/com/udea/web/banco/banco/BancoApplicationTests.java
+++ src/test/java/com/udea/web/banco/banco/BancoApplicationTests.java
@@ -1,16 +1,37 @@
 package com.udea.web.banco.banco;
 
+import com.udea.web.banco.banco.Controller.BankController;
+import com.udea.web.banco.banco.ModelMONGO.User;
+import com.udea.web.banco.banco.RepositoryMongo.UserRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.lang.reflect.Array;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@WebMvcTest(BankController.class)
 public class BancoApplicationTests {
+
+	@Autowired
+	UserRepository userRepository;
+
+	@Autowired
+	MockMvc mvc;
 
 	@Test
 	public void contextLoads() {
+
+		given(userRepository.findAll());
 	}
 
 }
